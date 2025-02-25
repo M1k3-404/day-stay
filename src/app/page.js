a4710a7bcf65ef39d4a6c6d9ccee8a87a24adae0
M1k3-404 <muthula.alwis@gmail.com>
2025-02-25 15:41:59 +0530

    SEO Update

--- src/app/page.js
+++ src/app/page.js
@@ -42,11 +42,11 @@ export default function Home() {
         autoPlay
         loop
         muted
-        src="/bg-temp.mp4"
+        src="/bg-1.mp4"
         className="absolute object-cover w-screen h-screen inset-0"
       />
 
-      <div className="absolute w-screen h-screen bg-white opacity-40 inset-0" />
+      <div className="absolute w-screen h-screen bg-white opacity-90 inset-0" />
 
       <div className="mx-auto w-3/4 md:w-1/2 opacity-100 z-10">
         <Image src="/Day-Stay Logo.png" width={300} height={300} alt="logo" />
@@ -55,22 +55,22 @@ export default function Home() {
         </p>
 
         <form onSubmit={handleSubmit} className="grid lg:grid-cols-3 gap-4 my-8">
-          <Input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} className="text-xs lg:text-base rounded-full placeholder:text-white" required />
-          <Input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} className="text-xs lg:text-base rounded-full placeholder:text-white" required />
-          <Button type="submit" className="rounded-full bg-[#db2879] hover:bg-[#db2879ef] font-medium text-xs" disabled={loading}>
+          <Input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} className="text-xs lg:text-base rounded-full" required />
+          <Input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} className="text-xs lg:text-base rounded-full" required />
+          <Button type="submit" className="rounded-full bg-[#db2879] hover:bg-[#db2879ef] font-medium text-xs lg:text-base" disabled={loading}>
             {loading ? "Registering..." : "Join Our Waitlist"}
           </Button>
         </form>
 
         <div className="flex gap-3 justify-center md:justify-start">
           <Link href={"https://www.facebook.com/people/DayStaylk/61571572059968/"}>
-            <FaFacebook className="text-lg text-[#db2879]"  />
+            <FaFacebook className="text-xl text-[#db2879]"  />
           </Link>
           <Link href={"https://www.instagram.com/daystay.lk/"}>
-            <FaInstagram className="text-lg text-[#db2879]" />
+            <FaInstagram className="text-xl text-[#db2879]" />
           </Link>
           <Link href={"https://www.tiktok.com/@daystay.lk"}>
-            <FaTiktok className="text-lg text-[#db2879]" />
+            <FaTiktok className="text-xl text-[#db2879]" />
           </Link>
         </div>
       </div>
