9978bcad9ce0b0f4c87fee5ecd444b286ab474bc
M1k3-404 <muthula.alwis@gmail.com>
2025-02-25 14:53:33 +0530

    Waitlist - Responsiveness

--- src/app/page.js
+++ src/app/page.js
@@ -37,32 +37,32 @@ export default function Home() {
   }
 
   return (
-    <div className="h-screen w-full bg-[#f8f8ff] flex justify-content items-center">
+    <div className="h-screen w-full bg-[#f8f8ff] flex justify-content items-center 2xl:px-72">
       <video
         autoPlay
         loop
         muted
         src="/bg-temp.mp4"
-        className="absolute object-cover w-full h-full inset-0"
+        className="absolute object-cover w-screen h-screen inset-0"
       />
 
-      <div className="absolute w-full h-full bg-white opacity-50 inset-o" />
+      <div className="absolute w-screen h-screen bg-white opacity-90 inset-0" />
 
-      <div className="mx-auto w-1/2 opacity-100 z-10">
+      <div className="mx-auto w-3/4 md:w-1/2 opacity-100 z-10">
         <Image src="/Day-Stay Logo.png" width={300} height={300} alt="logo" />
-        <p className="text-left mt-2 text-2xl font-semibold">
+        <p className="text-center md:text-left mt-2 text-sm md:text-base lg:text-2xl md:font-semibold">
           The finest day-use booking platform in Sri Lanka
         </p>
 
-        <form onSubmit={handleSubmit} className="grid grid-cols-3 gap-4 my-8">
-          <Input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} className="rounded-full placeholder:text-white" required />
-          <Input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} className="rounded-full placeholder:text-white" required />
-          <Button type="submit" className="rounded-full bg-[#db2879] hover:bg-[#db2879ef] font-medium" disabled={loading}>
-            {loading ? "Registering..." : "Join our Waitlist"}
+        <form onSubmit={handleSubmit} className="grid lg:grid-cols-3 gap-4 my-8">
+          <Input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} className="text-xs rounded-full" required />
+          <Input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} className="text-xs rounded-full" required />
+          <Button type="submit" className="rounded-full bg-[#db2879] hover:bg-[#db2879ef] font-medium text-xs" disabled={loading}>
+            {loading ? "Registering..." : "Join Our Waitlist"}
           </Button>
         </form>
 
-        <div className="flex gap-3">
+        <div className="flex gap-3 justify-center md:justify-start">
           <Link href={"https://www.facebook.com/people/DayStaylk/61571572059968/"}>
             <FaFacebook className="text-lg text-[#db2879]"  />
           </Link>
